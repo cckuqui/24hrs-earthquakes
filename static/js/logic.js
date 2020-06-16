ee98f3449cfef874cf8aaf00fb8190d3f17ef6fc
Cris B <cristina.bardan@gmail.com>
2020-06-16 17:52:40 -0500

    working legend not final version

--- static/js/logic.js
+++ static/js/logic.js
@@ -31,55 +31,14 @@ var layer_earthquakes = new L.LayerGroup();
 var usgsUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 var platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
-d3.json(usgsUrl, function(data) {
-
-    function popUp(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
-        "<p> Magnitude: " + feature.properties.mag + "</p>");
-    }
-    
-    function something(feature) {
-        var color ='';
-        if (feature.properties.mag > 5) {
-            color = '#4CC9F0';
-        } else if (feature.properties.mag > 4) {
-            color = '#4361EE';
-        } else if (feature.properties.mag > 3) {
-            color = '#3A0CA3';
-        } else if (feature.properties.mag > 2) {
-            color = '#7209B7';
-        } else if (feature.properties.mag > 1) {
-            color = '#B5179E';
-        } else {
-            color = '#F72585';
-        }
-    
-        var style = {
-            opacity: 1,
-            fillOpacity: 1,
-            color: "black",
-            fillColor: color,
-            radius: (feature.properties.mag * 8),
-            stroke: true,
-            weight: 1
-        }
-        
-        return style
-    }
-    
-    
-    L.geoJSON(data, {
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng);
-        },
-        style: something,
-        onEachFeature: popUp
-    }).addTo(layer_earthquakes);
-
-    layer_earthquakes.addTo(myMap);
-    // console.log('hi');
-});
+function color(d) {
+    return d > 5 ? '#fbb4ae' :
+           d > 4 ? '#b3cde3' :
+           d > 3 ? '#ccebc5' :
+           d > 2 ? '#decbe4' :
+           d > 1 ? '#fed9a6' :
+                    '#ffffcc';
+}
 
 d3.json(platesUrl, function (data){
         
@@ -92,21 +51,77 @@ d3.json(platesUrl, function (data){
     layer_plates.addTo(myMap);
 });
 
+d3.json(usgsUrl, function(data) {
+
+    function popUp(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
+        "<p> Magnitude: " + feature.properties.mag + "</p>");
+    }
+    
+    function something(feature) {  
+        var style = {
+            opacity: 1,
+            fillOpacity: 1,
+            color: "black",
+            fillColor: color(feature.properties.mag),
+            radius: (feature.properties.mag * 8),
+            stroke: true,
+            weight: 1
+        }
+        return style
+    }  
+    
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: something,
+        onEachFeature: popUp
+    }).addTo(layer_earthquakes);
+
+    layer_earthquakes.addTo(myMap);
+});
+
+// layer_earthquakes.bringToFront();
+
 var myMap = L.map("map", {
     center: [20, 0],
     zoom: 3,
     layers: [
         outdoorsmap, 
-        layer_earthquakes,
-        layer_plates
+        layer_plates,
+        layer_earthquakes  
     ]
 });
 
 var overlayMaps = {
-    Earthquakes: layer_earthquakes,
-    "Tectonic Plates": layer_plates
+    "Tectonic Plates": layer_plates,
+    Earthquakes: layer_earthquakes
 };
 
 L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
 }).addTo(myMap);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        mag = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+    div.innerHTML = "<h4>Earthquake<br>Magnitude</h4>";
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < mag.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + color(mag[i] + 1) + '"></i>' +
+            mag[i] + (mag[i + 1] ? ' &ndash; ' + mag[i + 1] + '<br>' : ' +');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
