53a4610545f86ca5c0d8f788683fe1e27ae86695
Cris B <cristina.bardan@gmail.com>
2020-07-16 23:12:46 -0500

    changed map

--- logic.js
+++ logic.js
@@ -14,17 +14,17 @@ var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/
     accessToken: key
 });
 
-var streetsmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var outdoorsmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox/streets-v11",
+    id: "mapbox/outdoors-v11",
     accessToken: key
 });
 
 var baseMaps = {
-    "Streets Map": streetsmap,
-    "Light Map": lightmap,
-    "Satellite Map": satellitemap    
+    "Satellite Map": satellitemap,
+    "Outdoors Map": outdoorsmap,
+    "Light Map": lightmap
 };
 
 var layer_plates = new L.LayerGroup();
